f490e304e512a45a8c51f601a1cd4e46330603f4
Yamlak Negash <yamlaknegash96@gmail.com>
2025-05-06 15:51:33 +0300

    Added Kotlin Statements and Some Modifiers

--- app/src/main/java/com/example/anilist/MainActivity.kt
+++ app/src/main/java/com/example/anilist/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.anilist.ui.theme.AniListTheme
@@ -35,34 +37,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ){
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
-
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary){
-        Column(modifier = modifier.padding(24.dp)) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 5.dp, horizontal = 10.dp)
+    ){
+        Column(modifier = modifier.fillMaxWidth().padding(vertical = 5.dp)) {
             Text(
                 text = "Hello ",
+                //I could have used this but not effective compared
+                //to adding it on the Column on different composables.
+//                modifier = Modifier
+//                    .padding(5.dp)
+//                    .fillMaxWidth()
             )
             Text(
-                text = name
+                text = name,
+//                modifier = Modifier
+//                    .padding(5.dp)
+//                    .fillMaxWidth()
             )
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     AniListTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
