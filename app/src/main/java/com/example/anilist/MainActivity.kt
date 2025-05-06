f147021fd6aee86c6128a64b530a282f34906422
Yamlak Negash <yamlaknegash96@gmail.com>
2025-05-06 16:57:08 +0300

    Before Button and Weight

--- app/src/main/java/com/example/anilist/MainActivity.kt
+++ app/src/main/java/com/example/anilist/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,25 +54,33 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded = remember { mutableStateOf(false) }
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 5.dp, horizontal = 10.dp)
     ){
-        Column(modifier = modifier.fillMaxWidth().padding(vertical = 5.dp)) {
-            Text(
-                text = "Hello ",
-                //I could have used this but not effective compared
-                //to adding it on the Column on different composables.
+        Row(modifier = modifier.padding(24.dp)){
+            Column(modifier = modifier.fillMaxWidth().padding(vertical = 5.dp)) {
+                Text(
+                    text = "Hello ",
+                    //I could have used this but not effective compared
+                    //to adding it on the Column on different composables.
 //                modifier = Modifier
 //                    .padding(5.dp)
 //                    .fillMaxWidth()
-            )
-            Text(
-                text = name,
+                )
+                Text(
+                    text = name,
 //                modifier = Modifier
 //                    .padding(5.dp)
 //                    .fillMaxWidth()
-            )
+                )
+            }
+            ElevatedButton(
+                onClick = {expanded.value = !expanded.value}
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
         }
     }
 }
