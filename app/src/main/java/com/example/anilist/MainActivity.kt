687d03bb007851e694be25c7c9a45e23552c6fdd
Yamlak Negash <yamlaknegash96@gmail.com>
2025-05-06 15:37:32 +0300

    Added Column Layout

--- app/src/main/java/com/example/anilist/MainActivity.kt
+++ app/src/main/java/com/example/anilist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.anilist.ui.theme.AniListTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,12 +35,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MyApp(modifier: Modifier = Modifier){
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ){
+        Greeting("Android")
+    }
+
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.primary){
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+        Column(modifier = modifier.padding(24.dp)) {
+            Text(
+                text = "Hello ",
+            )
+            Text(
+                text = name
+            )
+        }
     }
 }
 
